c67a48589375d555e199e04ac7132f4f480fab23
Mohammad Sabaeian <mohammad.sabaeian@gmail.com>
2017-11-27 13:43:13 +0330

    fix electron import error
    fix webpack for electron import error (fs) in react components

--- webpack.config.js
+++ webpack.config.js
@@ -98,4 +98,17 @@ module.exports = {
             },
         ],
     },
+    externals: [
+        (function () {
+            var IGNORES = [
+                'electron'
+            ];
+            return function (context, request, callback) {
+                if (IGNORES.indexOf(request) >= 0) {
+                    return callback(null, "require('" + request + "')");
+                }
+                return callback();
+            };
+        })()
+    ]
 };
